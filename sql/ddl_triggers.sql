69f3ee261bf886f02d8f38ccd200866f85b605aa
Matvey Arye <mat@timescale.com>
2017-10-30 06:54:30 -0400

    Remove ALTER EXTENSION from event trigger tag

--- sql/ddl_triggers.sql
+++ sql/ddl_triggers.sql
@@ -2,5 +2,7 @@ CREATE OR REPLACE FUNCTION _timescaledb_internal.ddl_command_end() RETURNS event
 AS 'MODULE_PATHNAME', 'timescaledb_ddl_command_end' LANGUAGE C IMMUTABLE STRICT;
 
 DROP EVENT TRIGGER IF EXISTS timescaledb_ddl_command_end;
+--EVENT TRIGGER MUST exclude the ALTER EXTENSION tag.
 CREATE EVENT TRIGGER timescaledb_ddl_command_end ON ddl_command_end
+WHEN TAG IN ('ALTER TABLE','CREATE TRIGGER','CREATE TABLE','CREATE INDEX','ALTER INDEX')
 EXECUTE PROCEDURE _timescaledb_internal.ddl_command_end();
